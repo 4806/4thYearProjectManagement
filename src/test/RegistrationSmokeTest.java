2e86f47b087182172719cc1a5836dbeaf59d59af
arsalansadiq <arsalansadiq@cmail.carleton.ca>
2019-03-01 18:32:32 -0500

    Added Tests

---
+++ src/test/RegistrationSmokeTest.java
@@ -0,0 +1,21 @@
+import app.controllers.RegistrationController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistrationSmokeTest {
+
+    @Autowired
+    private RegistrationController regController;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(regController).isNotNull();
+    }
+}
