01037e4d3050e191b0efe66d258e38e669b9f492
Josh Campitelli <joshcampitelli@cmail.carleton.ca>
2019-03-01 15:53:20 -0500

    PM-10: UC-1 Backend Registration functionality (#23)
    * Initialized CrudRepository
    
    * Added ThymeLeaf to Register Form:
    
    * Update registration.html
    
    * Removed Uneeded Dependancy

--- src/main/java/app/controllers/RegistrationController.java
+++ src/main/java/app/controllers/RegistrationController.java
@@ -1,14 +1,28 @@
 package app.controllers;
 
+import app.models.User;
+import app.models.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        model.addAttribute("user", new User());
         return "registration";
     }
 
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user) {
+        userRepository.save(user);
+        return "registration";
+    }
 }
