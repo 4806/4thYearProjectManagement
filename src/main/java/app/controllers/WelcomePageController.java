2d68c1dda47af20c2b175f8cc6199642fc6524d8
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2019-03-03 20:18:22 -0500

    small naming convention fix

--- src/main/java/app/controllers/WelcomePageController.java
+++ src/main/java/app/controllers/WelcomePageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomePageController{
     @RequestMapping("/")
-    public String LandingPage() {
+    public String welcome() {
         return "index";
     }
 }
