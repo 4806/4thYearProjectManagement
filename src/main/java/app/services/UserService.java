ceb5abd319f0197cbb8c182358c349e91db5069a
Abobakr Ba Mangar <abobakrbamangar@labs.sce.carleton.ca>
2019-03-03 21:49:03 -0500

    added verification to user role

--- src/main/java/app/services/UserService.java
+++ src/main/java/app/services/UserService.java
@@ -1,6 +1,26 @@
 package app.services;
 
-public interface UserService {
-    boolean authenticate(String username, String password);
+import app.models.User;
+import app.models.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean verifyNameAndPassword(String username, String password) {
+        // check username and password
+        User user = userRepository.findByUsername(username);
+        String u_name = user.getUsername();
+        String pass = user.getPassword();
+        return u_name.equals(username) && pass.equals(password);
+    }
+
+    boolean verifyUserRole (String username, String role){
+        User user = userRepository.findByUsername(username);
+        String dataBase_role = user.getRole();
+        return role.equals(dataBase_role);
+    }
 }
