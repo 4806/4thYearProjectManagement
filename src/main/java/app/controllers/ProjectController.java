affee723ffe05b5111824dc6506bc0c2bfba76c1
arsalansadiq <arsalan.sadiq.1996@gmail.com>
2019-03-19 12:51:41 -0400

    Fixed the issues with the merge, fixed the fail travis CI integration and refactored code

--- src/main/java/app/controllers/ProjectController.java
+++ src/main/java/app/controllers/ProjectController.java
@@ -32,24 +32,37 @@ public class ProjectController {
     }
 
     private List<Project> addProjectStubMethod() {
+        //program stub
+        Program program = new Program("Software Engineering", Program.Acronym.SOFT);
+        Program program1 =new Program("Computer Systems Engineering", Program.Acronym.COMP);
+        ArrayList<Program> programArrayList = new ArrayList<Program>();
+        programArrayList.add(program);
+        programArrayList.add(program1);
 
-        Supervisor supervisor = new Supervisor("Babak Esfandiari", "Password",new ArrayList<Project>(){{
-            add(new Project("Title Project", "Description Project", 5, null, null, null));
-        }});
-        ArrayList<Student> studentArrayList = new ArrayList<Student>(){{
-            add(new Student("Jacob Marshland","",null));
-            add(new Student("Jerry Burburaz","",null));
-        }};
+        // Students stubs
+        Student student = new Student("Jacob Marshland","","",null);
+        Student student1 = new Student("Jerry Burburaz","","",null);
+        ArrayList<Student> studentArrayList = new ArrayList<Student>();
+        studentArrayList.add(student);
+        studentArrayList.add(student1);
 
-        ArrayList<Program> programArrayList = new ArrayList<Program>(){{
-            add(new Program("Software Engineering", Program.Acronym.SOFT));
-            add(new Program("Computer Systems Engineering", Program.Acronym.COMP));
-        }};
+        //project stub
+        Project project3 = new Project("Title Project", "Description Project", 5, null, null, null);
+        ArrayList<Project> projectArrayList = new ArrayList<Project>();
+        projectArrayList.add(project3);
+
+        //supervisor stub
+        Supervisor supervisor = new Supervisor("Babak Esfandiari", "Password", "Password",projectArrayList);
+
+        //projects for list
+        Project project = new Project("Title Project", "Description Project", 4, supervisor, null, null);
+        Project project1 = new Project("Title Project", "Description Project", 2, null, studentArrayList, null);
+        Project project2 = new Project("Title Project", "Description Project", 65, null, null, programArrayList);
 
         List<Project> list = new ArrayList<Project>();
-        list.add(new Project("Title Project", "Description Project", 4, supervisor, null, null));
-        list.add(new Project("Title Project", "Description Project", 2, null, studentArrayList, null));
-        list.add(new Project("Title Project", "Description Project", 65, null, null, programArrayList));
+        list.add(project);
+        list.add(project1);
+        list.add(project2);
         return list;
     }
 
