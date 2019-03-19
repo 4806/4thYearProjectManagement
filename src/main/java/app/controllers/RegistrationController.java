6cfe44a87c4786028a93ef6dd906c7fd5b89d248
Josh Campitelli <joshcampitelli@cmail.carleton.ca>
2019-03-19 18:04:42 -0400

    PM-81: Update Repositories according to new Model (#88)
    * File restructure and project repositry
    
    * Program Repository
    
    * Fix issue

--- src/main/java/app/controllers/RegistrationController.java
+++ src/main/java/app/controllers/RegistrationController.java
@@ -1,14 +1,13 @@
 package app.controllers;
 
 import app.models.User;
-import app.models.UserRepository;
+import app.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
---
+++ src/main/java/app/repositories/ProgramRepository.java
@@ -0,0 +1,10 @@
+package app.repositories;
+
+import app.models.Program;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface ProgramRepository extends CrudRepository<Program, String> {
+    Program findByName(String name);
+}
