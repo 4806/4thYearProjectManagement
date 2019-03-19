e6b952ab2acebb257701548d320739741d9ffd40
joshcampitelli <joshcampitelli@cmail.carleton.ca>
2019-03-19 17:09:40 -0400

    Program Repository

---
+++ src/main/java/app/repositories/ProgramRepository.java
@@ -0,0 +1,11 @@
+package app.repositories;
+
+import app.models.Program;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+@Repository
+public interface ProgramRepository extends CrudRepository<Program, String> {
+    Program findByName(String name);
+    Program findAllAcronym(Program.Acronym acronym);
+}
