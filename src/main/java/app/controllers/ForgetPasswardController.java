ae25b4beb6078913c90b4b7b84e15ca61e7d3f56
Abobakr Ba Mangar <bamangar@gmail.com>
2019-03-18 22:57:22 -0400

    incomplete forgot password functionality

--- src/main/java/app/controllers/ForgetPasswardController.java
+++ src/main/java/app/controllers/ForgetPasswardController.java
@@ -1,12 +1,14 @@
 package app.controllers;
 
+import app.models.User;
 import app.models.UserRepository;
 import app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @Controller
 public class ForgetPasswardController {
@@ -15,12 +17,38 @@ public class ForgetPasswardController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/forgot")
+    @RequestMapping(value = "/forgot",method = RequestMethod.GET)
     public String dispalyforgotPasswordForm(Model model){
+        model.addAttribute("user", new User());
         return "forgotPassword";
     }
-    @PostMapping("/forgot")
-    public String processPasswordForm(){
+
+    @RequestMapping(value = "/forgot",method = RequestMethod.POST)
+    public String processPasswordForm(@ModelAttribute User user, Model model ){
+        String  username = user.getUsername();
+         User DBuser = userRepository.findByUsername(username);
+         if (DBuser ==null) {
+             // user do not have an account
+             return "redirect:";
+         }
+         // ToDO: check the user 's answer for the security question
+        // if(DBuser.getAnswerToSecurityQuestion().equals(user.getAnswerToSecurityQuestion())){
+             //return "redirect:reset";
+        // }
+        return "redirect:reset";
+    }
+
+    @RequestMapping(value ="/reset",method = RequestMethod.GET)
+    public String dispalyresetPasswordForm(Model model){
+        model.addAttribute("user", new User());
+        return "resetPassword";
+    }
+    @RequestMapping(value = "/reset",method = RequestMethod.POST)
+    public String processResetPasswordForm(@ModelAttribute User user){
+        if(user.getPassword().equals(user.getConfPassword())){
+            userRepository.save(user);
+            return "redirect:login";
+        }
         return "resetPassword";
     }
 
