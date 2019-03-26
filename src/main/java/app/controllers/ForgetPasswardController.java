4ca2347ba692413d84c154ca1bf9dacfbf217e40
Abobakr Ba Mangar <bamangar@gmail.com>
2019-03-25 20:16:55 -0400

    Fixed bug for looged in users

--- src/main/java/app/controllers/ForgetPasswardController.java
+++ src/main/java/app/controllers/ForgetPasswardController.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Controller
 public class ForgetPasswardController {
@@ -23,6 +26,12 @@ public class ForgetPasswardController {
 
     @RequestMapping(value = "/forgot",method = RequestMethod.GET)
     public String dispalyforgotPasswordForm(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            /* The user is logged in :) */
+            return "redirect:";
+        }
+        // dispaly forgot password form
         model.addAttribute("user", new User());
         model.addAttribute("view", "forgotPassword");
         return "layout";
@@ -35,9 +44,12 @@ public class ForgetPasswardController {
         // if  no account with username  provided exists print username error message
         if (resetUser ==null) {
             model.addAttribute("view", "forgotPassword");
+            // error message will be dsiplayed when wrong username is provided
             model.addAttribute("usernameError", true);
              return "layout";
         }
+        // when the user name is valid check the answer to security question
+        // if correct allow the user to reset their password otherwise rturn an error message
         if(resetUser.getAnswerTosecurityQuestion().equals(user.getAnswerTosecurityQuestion())){
             if(user.getPassword().equals(user.getConfPassword())){
                 resetUser.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -47,12 +59,14 @@ public class ForgetPasswardController {
                 model.addAttribute("resetSuccess", true);
                 return "layout";
             }else {
+                // if password do not match return an error message.
                 model.addAttribute("view", "forgotPassword");
                 model.addAttribute("pwError", true);
                 return "layout";
             }
         }else {
             model.addAttribute("view", "forgotPassword");
+            // error message when answer to security question is wrong
             model.addAttribute("answerError", true);
             return "layout";
         }
